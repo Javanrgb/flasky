21f5c0662279b3978de55320700d897442c7ce1f
javan <ochiengjavan133@gmail.com>
2023-02-02 05:59:22 +0300

    second commit for python tutorial

--- myvenv/bin/pythonfiles/my_modules.py
+++ myvenv/bin/pythonfiles/my_modules.py
@@ -1,12 +1,25 @@
-print("Imported my_modules")
+"""print("Imported my_modules")
 
 test = "Test String"
 
 
 def find_index(to_search, target):
-    """finds index of a value"""
+    #finds index of a value
     for i, value in enumerate(to_search):
         if value == target:
             return i
 
     return -1
+
+import random
+
+courses = ['Math', 'Art','Biology','CompSci', 'Physics']
+random_course = random.choice(courses)
+print(random_course)
+
+import math
+rads = math.sin(math.radians(78))
+print(rads)
+"""
+import antigravity
+
